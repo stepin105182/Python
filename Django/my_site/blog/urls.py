e37c8ad896d49d3c7c931212cf20031ca0c30aea
Vikram Hegde <vikram.anandnarayanhegde@external.marelli.com>
2024-07-24 19:13:27 +0530

    day2

---
+++ Django/my_site/blog/urls.py
@@ -0,0 +1,15 @@
+from . import views
+from django.urls import path
+
+
+app_name = 'blog'
+     
+
+urlpatterns = [
+    path("",views.start,name="start"),
+    path("posts",views.allposts,name="all-posts"),
+    path("contact",views.contact,name="contact"),
+    path("posts/<slug:slug>",views.post,name="post"),
+
+
+]
---
+++ Django/my_site/blog/views.py
@@ -0,0 +1,21 @@
+
+from django.shortcuts import render
+
+from django.contrib import messages
+
+# Create your views here.
+
+def start(request):
+    return render(request,"blog/index.html")
+
+def allposts(request):
+    return render(request,"blog/index.html")
+
+def post(request):
+    return "ho"
+
+def contact(request):
+    if request.method == 'POST':
+        messages.success(request, "Message sent.")
+
+    return render(request,"blog/contact.html")
